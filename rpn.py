7476aabe47b13cc7546015e97dbb15f14b740d4f
deannajs <deannajs@umich.edu>
2018-02-16 12:12:02 -0500

    Second commit for rpn.py and test_rpn.py and makefile

--- rpn.py
+++ rpn.py
@@ -1,11 +1,36 @@
-#/usr/bin/env python3
+#!/usr/bin/env python3
+
+import operator
+
+operators = {
+	'+': operator.add,
+	'-': operator.sub,
+	'*': operator.mul,
+	'/': operator.truediv,
+}
+
 
 def calculate(arg):
-	pass
+	stack = list()
+	for token in arg.split():
+		try:
+			value = int(token)
+			stack.append(value)
+		except ValueError:
+			function = operators[token]
+			arg2 = stack.pop()
+			arg1 = stack.pop()
+			result = function(arg1, arg2)
+			stack.append(result)
+		print(stack)
+	if len(stack) != 1:
+		raise TypeError("Too many parameters")
+	return stack.pop()
+
 
 def main():
 	while True:
-		calculate(input('rpn calc> '))
+		print(calculate(input('rpn calc> ')))
 
 if __name__ == '__main__':
 	main()
