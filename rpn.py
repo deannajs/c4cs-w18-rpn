d7af67d9d14ccc9b94fb1cadbcae079ab01b6bbc
deannajs <deannajs@umich.edu>
2018-03-06 12:56:54 -0500

    adding .travis.yml file

--- rpn.py
+++ rpn.py
@@ -1,13 +1,39 @@
 #!/usr/bin/env python3
 
 import operator
+import logging
+import sys
+import math
+
+
+def pplus(first, second):
+	result = first * second / 100
+	return first + result
+
+def pminus(first, second):
+	result = first * second / 100
+	return first - result
+	
 
 operators = {
 	'+': operator.add,
 	'-': operator.sub,
 	'*': operator.mul,
 	'/': operator.truediv,
+	'^': operator.pow,
+	'%+': pplus,
+	'%-': pminus,
+	'.': operator.floordiv,
+	'&': operator.and_,
+	'|': operator.or_,
+	'~': operator.invert,
+	'!': math.factorial,
 }
+#logger = logging.getLogger(__name__)
+#logger.setLevel(logging.DEBUG)
+#sh = logging.StreamHandler(sys.stdout)
+#logger.addHandler(sh)
+#logger.basicConfig(level=logging.DEBUG)
 
 
 def calculate(arg):
@@ -17,16 +43,28 @@ def calculate(arg):
 			value = int(token)
 			stack.append(value)
 		except ValueError:
-			function = operators[token]
-			arg2 = stack.pop()
-			arg1 = stack.pop()
-			result = function(arg1, arg2)
-			stack.append(result)
+			if (token == '~'):
+				function = operators[token]
+				arg1 = stack.pop()
+				result = function(arg1)
+				stack.append(result)	
+			elif (token == '!'):
+				function = operators[token]
+				arg1 = stack.pop()
+				result = function(arg1)
+				stack.append(result)
+
+			else:
+				function = operators[token]
+				arg2 = stack.pop()
+				arg1 = stack.pop()
+				result = function(arg1, arg2)
+				stack.append(result)
 		print(stack)
 	if len(stack) != 1:
 		raise TypeError("Too many parameters")
 	return stack.pop()
-
+	#logger.debug(stack)
 
 def main():
 	while True:
--- test_rpn.py
+++ test_rpn.py
@@ -2,16 +2,42 @@ import unittest
 import rpn
 
 class TestBasics(unittest.TestCase):
-	def test_add(self):
-		result = rpn.calculate("1 1 +")
-		self.assertEqual(2, result)
-	def test_subtract(self):
-		result = rpn.calculate("5 3 -")
-		self.assertEqual(2, result)
-	def test_multiply(self):
-		result = rpn.calculate("5 3 *")
-		self.assertEqual(15, result)
-	def test_divide(self):
-		result = rpn.calculate("6 3 /")
-		self.assertEqual(2, result)
-
+	#def test_add(self):
+	#	result = rpn.calculate("1 1 +")
+	#	self.assertEqual(2, result)
+	#def test_subtract(self):
+	#	result = rpn.calculate("5 3 -")
+	#	self.assertEqual(2, result)
+	#def test_multiply(self):
+	#	result = rpn.calculate("5 3 *")
+	#	self.assertEqual(15, result)
+	#def test_divide(self):
+	#	result = rpn.calculate("6 3 /")
+	#	self.assertEqual(2, result)
+	#def test_toomany(self):
+	#	with self.assertRaises(TypeError):
+	#		result = rpn.calculate("1 2 3 +")
+	#def test_pow(self):
+	#	result = rpn.calculate("2 3 ^")
+	#	self.assertEqual(8, result)
+	#def test_percentplus(self):
+	#	result = rpn.calculate("72 5 %+")
+	#	self.assertEqual(75.6, result)
+	#def test_percentminus(self):
+	#	result = rpn.calculate("72 20 %-")
+	#	self.assertEqual(57.6, result)
+	#def test_intdivision(self):
+	#	result = rpn.calculate("10 3 .")
+	#	self.assertEqual(3, result)
+	#def test_and(self):
+	#	result = rpn.calculate("10 3 &")
+	#	self.assertEqual(2, result)
+	#def test_or(self):
+	#	result = rpn.calculate("10 3 |")
+	#	self.assertEqual(11, result)
+	#def test_not(self):
+	#	result = rpn.calculate("-6 ~")
+	#	self.assertEqual(5, result)
+	def test_factorial(self):
+		result = rpn.calculate("4 !")
+		self.assertEqual(24, result)
