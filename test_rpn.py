e3745b1405df1b82e774f078edf53620fdb46434
deannajs <deannajs@umich.edu>
2018-03-06 13:08:04 -0500

    updated test_rpn.py with power ^ function test

--- test_rpn.py
+++ test_rpn.py
@@ -2,24 +2,24 @@ import unittest
 import rpn
 
 class TestBasics(unittest.TestCase):
-	#def test_add(self):
-	#	result = rpn.calculate("1 1 +")
-	#	self.assertEqual(2, result)
-	#def test_subtract(self):
-	#	result = rpn.calculate("5 3 -")
-	#	self.assertEqual(2, result)
-	#def test_multiply(self):
-	#	result = rpn.calculate("5 3 *")
-	#	self.assertEqual(15, result)
-	#def test_divide(self):
-	#	result = rpn.calculate("6 3 /")
-	#	self.assertEqual(2, result)
-	#def test_toomany(self):
-	#	with self.assertRaises(TypeError):
-	#		result = rpn.calculate("1 2 3 +")
-	#def test_pow(self):
-	#	result = rpn.calculate("2 3 ^")
-	#	self.assertEqual(8, result)
+	def test_add(self):
+		result = rpn.calculate("1 1 +")
+		self.assertEqual(2, result)
+	def test_subtract(self):
+		result = rpn.calculate("5 3 -")
+		self.assertEqual(2, result)
+	def test_multiply(self):
+		result = rpn.calculate("5 3 *")
+		self.assertEqual(15, result)
+	def test_divide(self):
+		result = rpn.calculate("6 3 /")
+		self.assertEqual(2, result)
+	def test_toomany(self):
+		with self.assertRaises(TypeError):
+			result = rpn.calculate("1 2 3 +")
+	def test_pow(self):
+		result = rpn.calculate("2 3 ^")
+		self.assertEqual(8, result)
 	#def test_percentplus(self):
 	#	result = rpn.calculate("72 5 %+")
 	#	self.assertEqual(75.6, result)
